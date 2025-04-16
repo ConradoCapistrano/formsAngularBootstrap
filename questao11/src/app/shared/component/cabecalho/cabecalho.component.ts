f28279c3c91196510721c504613778d70370c48f
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-16 17:43:18 -0300

    feat: finalizar cabecalho component

--- questao11/src/app/shared/component/cabecalho/cabecalho.component.ts
+++ questao11/src/app/shared/component/cabecalho/cabecalho.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './cabecalho.component.css'
 })
 export class CabecalhoComponent {
-
+  readonly logoPath: string = '../../../../../assets/imagens/logo-memoteca.png';
+  readonly logoAltText: string = 'Logo da aplicação Memoteca';
+  readonly computerImagePath: string = '/assets/imagens/computador-cabecalho.png';
+  readonly computerImageAltText: string = 'Imagem de um computador';
+  readonly descriptionText: string = 'Guarde trechos de músicas, citações de livros, pensamentos e suas melhores ideias.';
 }
