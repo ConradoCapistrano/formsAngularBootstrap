b36e1a5d4407780da04c15aef36ed715acebc254
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-19 17:07:34 -0300

    perf: melhora performance da aplicacao com alteracoes em todos os arquivos commitados

--- questao10/src/app/app.component.ts
+++ questao10/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'questao10';
+
+  usuarioAtivo: boolean | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  verificarStatusUsuario() {
+    this.http.post<{usuarioAtivo: boolean}>('usuario/ativo', {})
+      .subscribe({
+        next: (response) => {
+          this.usuarioAtivo = response.usuarioAtivo;
+        },
+        error: (err) => {
+          console.error('Erro ao verificar status do usuário:', err);
+          this.usuarioAtivo = null;
+        }
+      });
+  }
 }
