e2fed8ac6954422897b51c7e19d32c6318a688a4
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-19 17:11:59 -0300

    feat: adiciona implementacoes para deixar o component mais dinamico

--- questao11/src/app/pages/mural/mural.component.ts
+++ questao11/src/app/pages/mural/mural.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Formulario } from '../formulario/formulario';
+import Swal from 'sweetalert2';
+import { FormularioService } from '../../shared/formulario.service';
+import { FormularioComponent } from "../formulario/formulario.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mural',
   standalone: true,
-  imports: [],
+  imports: [FormularioComponent, FormsModule, CommonModule, RouterLink],
   templateUrl: './mural.component.html',
   styleUrl: './mural.component.css'
 })
-export class MuralComponent {
+export class MuralComponent implements OnInit{
+  mural: Formulario[] = [];
 
+  constructor(private service: FormularioService) { }
+
+  ngOnInit(): void {
+    this.carregarPensamento();
+  }
+
+  carregarPensamento() {
+    this.service.listar().subscribe((mural) => {
+      this.mural = mural;
+    });
+  }
+
+  onCardExcluido(id: number) {
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Você não poderá reverter esta ação!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, excluir!",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.excluir(id).subscribe({
+          next: () => {
+            Swal.fire({
+              title: "Excluído!",
+              text: "O pensamento foi excluído com sucesso.",
+              icon: "success",
+              confirmButtonColor: "#3085d6",
+            });
+            this.carregarPensamento();
+          },
+          error: (erro) => {
+            Swal.fire({
+              title: "Erro!",
+              text: "Ocorreu um erro ao excluir o pensamento.",
+              icon: "error",
+              confirmButtonColor: "#d33",
+            });
+            console.error(erro);
+          }
+        });
+      }
+    });
+  }
 }
