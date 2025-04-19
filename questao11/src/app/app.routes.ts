02e55ecae1f0e2c48ea8a301b253031d3bfed03d
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-19 17:10:36 -0300

    perf: adiciona lazy loading em rotas para otimizar carregamento da aplicação

--- questao11/src/app/app.routes.ts
+++ questao11/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MuralComponent } from './pages/mural/mural.component';
+import { CriarFormularioComponent } from './pages/criar-formulario/criar-formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mural',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-formulario',
+    loadComponent: () => import('./pages/criar-formulario/criar-formulario.component').then(m => m.CriarFormularioComponent)
+  },
+  {
+    path: 'mural',
+    component: MuralComponent
+  },
+  {
+    path: 'editar-formulario/:id',
+    loadComponent: () => import('./pages/editar-formulario/editar-formulario.component').then(m => m.EditarFormularioComponent)
+  }
+]
