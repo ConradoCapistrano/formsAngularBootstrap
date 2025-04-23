cef643d73b3fa537c1d4b2347a3f3c2b0cbbee09
Conrado Capistrano <capistrano.conrado@gmail.com>
2025-04-23 08:55:52 -0300

    refactor: padronizar parametros para se adaptar ao backend

--- questao11/src/app/shared/formulario.service.ts
+++ questao11/src/app/shared/formulario.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Formulario } from '../pages/formulario/formulario';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Formulario, ApiResponse } from '../pages/formulario/formulario';
+import { Observable, catchError, throwError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormularioService {
 
-  private readonly API = 'http://localhost:3000/pensamentos'
+  private readonly API = 'https://localhost:7105/api/pensamentos'
 
   constructor(private http: HttpClient) { }
 
   listar(): Observable<Formulario[]> {
-    return this.http.get<Formulario[]>(this.API).pipe(
+    return this.http.get<any>(this.API).pipe(
+      map(response => {
+        if (Array.isArray(response)) {
+          return response;
+        }
+        else if (response && Array.isArray(response.data)) {
+          return response.data;
+        }
+        return [];
+      }),
       catchError(this.handleError)
     );
   }
@@ -21,16 +30,16 @@ export class FormularioService {
     return this.http.post<Formulario>(this.API, formulario)
   }
   editar(formulario: Formulario): Observable<Formulario> {
-    const url = `${this.API}/${formulario.id}`
-    return this.http.put<Formulario>(url, formulario )
+    return this.http.put<Formulario>(this.API, formulario).pipe(
+      catchError(this.handleError)
+    );
   }
   excluir(id: number): Observable<Formulario> {
     const url = `${this.API}/${id}`
     return this.http.delete<Formulario>(url)
   }
-  obterPorId(id: number): Observable<Formulario> {
-    const url = `${this.API}/${id}`
-    return this.http.get<Formulario>(url)
+  obterPorId(id: number): Observable<ApiResponse<Formulario>> {
+    return this.http.get<ApiResponse<Formulario>>(`${this.API}/${id}`);
   }
   private handleError(error: any): Observable<never> {
     console.error('Erro na API', error);
